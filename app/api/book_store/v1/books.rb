b70f83acf473a919ce047be33b26470b69784edc
kmvandna <vandna_gola@qwertycode.in>
2022-05-20 13:02:26 +0530

    add gem file and create api folder

---
+++ app/api/book_store/v1/books.rb
@@ -0,0 +1,18 @@
+module BookStore
+  module V1
+    class Books < Grape::API
+      version 'v1', using: :path
+      format :json
+      prefix :api
+      resource :books do
+        desc 'Return list of books'
+        get do
+          books = Book.all
+          present books
+        end
+      end
+    end
+  end
+end
+
+
